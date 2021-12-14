02557cf3192ce3d9840f2ebc210c4376f3c25c88
Talkarcabbage <talkarcabbage@protonmail.com>
2021-12-14 16:55:17 -0600

    Kotlinified

---
+++ src/main/kotlin/io/github/talkarcabbage/materialtech/MaterialTech.kt
@@ -0,0 +1,10 @@
+package io.github.talkarcabbage.materialtech
+
+import net.fabricmc.api.ModInitializer
+
+object MaterialTech : ModInitializer{
+    override fun onInitialize() {
+        println("Initializing the material tech mod let's go")
+    }
+
+}
